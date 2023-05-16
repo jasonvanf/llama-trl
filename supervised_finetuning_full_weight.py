68f61bfa2458c3f6c6e3cf3b6f47346c3bb987c9
jasonvan <jasondurianvan@gmail.com>
2023-05-16 10:37:56 +0800

    Support full weight fine-tuning with DeepSpeed stage-3 (offload)

---
+++ supervised_finetuning_full_weight.py
@@ -0,0 +1,223 @@
+import os
+import argparse
+
+from tqdm import tqdm
+from datasets import load_dataset
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    LlamaTokenizer,
+    TrainingArguments,
+    logging,
+    set_seed
+)
+from trl import SFTTrainer
+from trl.trainer import ConstantLengthDataset
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--base_model", type=str, default="")
+    parser.add_argument("--dataset_name", type=str, default="./data/alpaca_gpt4_data.json")
+    parser.add_argument("--split", type=str, default="train")
+    parser.add_argument("--size_valid_set", type=int, default=4000)
+    parser.add_argument("--streaming", action="store_true", default=False)
+    parser.add_argument("--shuffle_buffer", type=int, default=5000)
+
+    parser.add_argument("--seq_length", type=int, default=1024)
+    parser.add_argument("--max_steps", type=int, default=10000)
+    parser.add_argument("--batch_size", type=int, default=16)
+    parser.add_argument("--gradient_accumulation_steps", type=int, default=1)
+    parser.add_argument("--eos_token_id", type=int, default=49152)
+
+    parser.add_argument("--learning_rate", type=float, default=1e-4)
+    parser.add_argument("--lr_scheduler_type", type=str, default="linear")
+    parser.add_argument("--num_warmup_steps", type=int, default=100)
+    parser.add_argument("--weight_decay", type=float, default=0.05)
+    parser.add_argument("--warmup_ratio", type=float, default=0.)
+    parser.add_argument("--deepspeed", type=str, default=None)
+
+    parser.add_argument("--local_rank", type=int, default=0)
+    parser.add_argument("--fp16", action="store_true", default=False)
+    parser.add_argument("--no_bf16", action="store_false", default=True)
+    parser.add_argument("--no_gradient_checkpointing", action="store_false", default=True)
+    parser.add_argument("--seed", type=int, default=1103)
+    parser.add_argument("--num_workers", type=int, default=None)
+    parser.add_argument("--output_dir", type=str, default="./checkpoints/supervised_llama/")
+    parser.add_argument("--log_freq", default=1, type=int)
+    parser.add_argument("--eval_freq", default=1000, type=int)
+    parser.add_argument("--save_freq", default=1000, type=int)
+    parser.add_argument("--save_total_limit", default=3, type=int)
+    parser.add_argument("--run_name", default="llama-supervised-finetuned", type=str)
+
+    return parser.parse_args()
+
+
+def chars_token_ratio(dataset, tokenizer, nb_examples=400):
+    """
+    Estimate the average number of characters per token in the dataset.
+    """
+    total_characters, total_tokens = 0, 0
+    for _, example in tqdm(zip(range(nb_examples), iter(dataset)), total=nb_examples):
+        text = prepare_sample_text(example)
+        total_characters += len(text)
+        if tokenizer.is_fast:
+            total_tokens += len(tokenizer(text).tokens())
+        else:
+            total_tokens += len(tokenizer.tokenize(text))
+
+    return total_characters / total_tokens
+
+
+def prepare_sample_text(data_point):
+    """Prepare the text from a sample of the dataset."""
+    if data_point["input"]:
+        return f"""Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
+
+### Instruction:
+{data_point["instruction"]}
+
+### Input:
+{data_point["input"]}
+
+### Response:
+{data_point["output"]}"""
+    else:
+        return f"""Below is an instruction that describes a task. Write a response that appropriately completes the request.
+
+### Instruction:
+{data_point["instruction"]}
+
+### Response:
+{data_point["output"]}"""
+
+
+def create_datasets(tokenizer, args):
+    data_path = args.dataset_name
+    data_kwargs = {
+        "split": args.split,
+        "num_proc": args.num_workers if not args.streaming else None,
+        "streaming": args.streaming,
+    }
+    if data_path.endswith(".json") or data_path.endswith(".jsonl"):
+        dataset = load_dataset("json", data_files=data_path, **data_kwargs)
+    else:
+        dataset = load_dataset(data_path, **data_kwargs)
+
+    if args.streaming:
+        print("Loading the dataset in streaming mode")
+        valid_data = dataset.take(args.size_valid_set)
+        train_data = dataset.skip(args.size_valid_set)
+        train_data = train_data.shuffle(buffer_size=args.shuffle_buffer, seed=args.seed)
+    else:
+        dataset = dataset.train_test_split(test_size=0.1, seed=args.seed)
+        train_data = dataset["train"]
+        valid_data = dataset["test"]
+        print(f"Size of the train set: {len(train_data)}. Size of the validation set: {len(valid_data)}")
+
+    chars_per_token = chars_token_ratio(train_data, tokenizer)
+    print(f"The character to token ratio of the dataset is: {chars_per_token:.2f}")
+
+    train_dataset = ConstantLengthDataset(
+        tokenizer,
+        train_data,
+        formatting_func=prepare_sample_text,
+        infinite=True,
+        seq_length=args.seq_length,
+        chars_per_token=chars_per_token,
+    )
+    valid_dataset = ConstantLengthDataset(
+        tokenizer,
+        valid_data,
+        formatting_func=prepare_sample_text,
+        infinite=False,
+        seq_length=args.seq_length,
+        chars_per_token=chars_per_token,
+    )
+    return train_dataset, valid_dataset
+
+
+def run_training(args, train_data, val_data, tokenizer=None):
+    print("Loading the model")
+
+    train_data.start_iteration = 0
+
+    print("Starting main loop")
+
+    training_args = TrainingArguments(
+        output_dir=args.output_dir,
+        dataloader_drop_last=True,
+        evaluation_strategy="steps",
+        max_steps=args.max_steps,
+        eval_steps=args.eval_freq,
+        save_steps=args.save_freq,
+        logging_steps=args.log_freq,
+        save_total_limit=args.save_total_limit,
+        per_device_train_batch_size=args.batch_size,
+        per_device_eval_batch_size=args.batch_size,
+        learning_rate=args.learning_rate,
+        lr_scheduler_type=args.lr_scheduler_type,
+        warmup_steps=args.num_warmup_steps,
+        gradient_accumulation_steps=args.gradient_accumulation_steps,
+        gradient_checkpointing=args.no_gradient_checkpointing,
+        fp16=args.fp16,
+        bf16=args.no_bf16,
+        weight_decay=args.weight_decay,
+        warmup_ratio=args.warmup_ratio,
+        deepspeed=args.deepspeed,
+        run_name=args.run_name,
+        report_to="wandb",
+        ddp_find_unused_parameters=False if int(os.environ.get("WORLD_SIZE", 1)) != 1 else None,
+    )
+
+    model = AutoModelForCausalLM.from_pretrained(
+        args.base_model,
+    )
+
+    trainer = SFTTrainer(
+        model=model,
+        tokenizer=tokenizer,
+        args=training_args,
+        train_dataset=train_data,
+        eval_dataset=val_data,
+        packing=True,
+    )
+
+    print("Training...")
+    trainer.train()
+
+    print("Saving last checkpoint of the model")
+    lora_model_path = os.path.join(args.output_dir, "final_checkpoint/")
+    trainer.model.save_pretrained(lora_model_path)
+
+
+def main(args):
+    if "decapoda" in args.base_model.lower():
+        tokenizer = LlamaTokenizer.from_pretrained(args.base_model)
+        tokenizer.add_special_tokens(
+            {
+                "eos_token": "</s>",
+                "bos_token": "</s>",
+                "unk_token": "</s>",
+                "pad_token": "</s>",
+            }
+        )
+    else:
+        tokenizer = AutoTokenizer.from_pretrained(args.base_model, use_fast=False)
+        if getattr(tokenizer, "pad_token", None) is None:
+            tokenizer.pad_token = tokenizer.eos_token
+
+    train_dataset, eval_dataset = create_datasets(tokenizer, args)
+    run_training(args, train_dataset, eval_dataset, tokenizer)
+
+
+if __name__ == "__main__":
+    args = get_args()
+    assert args.base_model != "", "Please provide the llama model path"
+
+    set_seed(args.seed)
+    os.makedirs(args.output_dir, exist_ok=True)
+
+    logging.set_verbosity_error()
+
+    main(args)
